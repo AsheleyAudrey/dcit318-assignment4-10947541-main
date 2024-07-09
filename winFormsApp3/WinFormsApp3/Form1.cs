3d7e79c505925f5e2d09c6d5e45f2319a7cfcb6a
AsheleyAudrey <aaamarh005@st.ug.edu.gh>
2024-07-09 12:04:48 +0000

    Create a Windows Forms application that allows users to draw ona canvas

---
+++ winFormsApp3/WinFormsApp3/Form1.cs
@@ -0,0 +1,35 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
